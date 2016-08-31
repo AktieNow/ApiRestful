88a5af8c55b3281bd5114b67fd3a54f33fb9dec5
Moises Pereira <mpereira@aktienow.com>
2016-08-31 12:06:20 -0300

    adicionado envio e retorno

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/servidor', function(req, res) {
-  console.log('req.params', req);
-  res.send('Meet11 - ' + req.query.nome );
-  //res.writeHead(301, {Location: 'http://localhost:3000/meet'});
-  //res.end();
+app.get('/envia', function(req, res) {
+
+   http.get('http://192.168.1.108:3000/recebe?nome=Moises', function(data){
+      console.log('foi');
+   });
+   res.end();
+
 });
 
-/*app.get('/server', function(req, res) {
-  console.log('aki');
-  res.send('A rota esta certa - ');
-});*/
 
-//app.use('meet/', require('./api/meet.js'));
+app.get('/retorna', function(req, res) {
+
+    fs.writeFile("retorno_api.txt", req.query.nome, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Arquivo salvo");
+    });
+
+    res.send(req.query.nome);
+});
 
 var server = app.listen(3000, function(){
 	var host = server.address().address
@@ -22,23 +30,3 @@ var server = app.listen(3000, function(){
 
 	console.log('listening.... ', host, port);
 });
-
-/*const express = require('express');
-
-var app = express();
-
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");//TODO subdominios que podem ter acesso
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', "X-Requested-With");
-    next();
-});
-
-app.use('meet/', require('./api/meet.js'));
-
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-*/
